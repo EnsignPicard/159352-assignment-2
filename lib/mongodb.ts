37949989a738ed5b8bda6f1f3ac8cb38d4bc2c15
Jesse Jacobs <jessejjacobs93@gmail.com>
2026-05-22 15:46:14 +1200

    first commit

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,29 +1,8 @@
-/*
- * Need a way of checking for existing connections
- * that works on Vercel
- */
+import { MongoClient } from "mongodb";
+const URI = process.env.MONGODB_URI!;
+const DB_NAME = "sample_mflix";
 const client = new MongoClient(URI);
-
 export async function connectDB() {
-  // Do this check to avoid re-connecting - doesn't work on Vercel
-  /*
-  if (!client) {
-    console.log('New connection');
-    await client.connect();
-  }
-  */
-
-  /*
-  const globalWithMongo = global as typeof globalThis & { _mongoClient?: MongoClient };
-
-  if (!globalWithMongo._mongoClient) {
-    globalWithMongo._mongoClient = new MongoClient(URI);
-  }
-
-  const client = globalWithMongo._mongoClient;
-  */
-
-  // Get the named database object
-  const db = await client.db(DB_NAME);
-  return db;
+    const db = await client.db(DB_NAME);
+    return db;
 }
